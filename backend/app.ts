3ceb88bc3bd5eea52e52f0510d88ba88d9b508c4
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-07-16 18:46:03 +0200

    feat: configured app

---
+++ backend/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import toDORoute from "./route/toDo";
+import { errorRoute } from "./route/errorRoute";
+import { NotFoundError } from "./route/notFoundError";
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  }),
+);
+
+app.use("/api", toDORoute);
+
+app.use(NotFoundError);
+
+app.use(errorRoute);
+
+export default app;
