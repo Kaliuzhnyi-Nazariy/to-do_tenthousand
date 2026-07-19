dec65cd270be5cc35c31722e73df74720cf96997
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-07-19 14:27:31 +0200

    fix: add backend url

--- frontend/features/api.ts
+++ frontend/features/api.ts
@@ -1,11 +1,16 @@
 import axios from "axios";
 
 const NEXT_PUBLIC_BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+const NEXT_PUBLIC_LOCAL_BACKEND_URL = process.env.NEXT_PUBLIC_LOCAL_BACKEND_URL;
+const NEXT_PUBLIC_ENVIRONMENT = process.env.NEXT_PUBLIC_ENVIRONMENT;
 
 if (!NEXT_PUBLIC_BACKEND_URL) {
   throw new Error("no backend url");
 }
 
 export const api = axios.create({
-  baseURL: NEXT_PUBLIC_BACKEND_URL,
+  baseURL:
+    NEXT_PUBLIC_ENVIRONMENT === "production"
+      ? NEXT_PUBLIC_BACKEND_URL
+      : NEXT_PUBLIC_LOCAL_BACKEND_URL,
 });
