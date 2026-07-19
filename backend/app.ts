50f6fe6c11d89141c18e7e17ccf360b7b7f452a7
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-07-19 16:05:51 +0200

    add frontend link

--- backend/app.ts
+++ backend/app.ts
@@ -6,10 +6,12 @@ import { NotFoundError } from "./route/notFoundError";
 
 const app = express();
 
+const { FRONTEND_URL } = process.env;
+
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", FRONTEND_URL!],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   }),
 );
