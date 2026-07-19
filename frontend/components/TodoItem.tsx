ed8ef31afc713607cfbf77f9ca8db5ba255f4bca
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-07-19 13:18:44 +0200

    feat: list of todos

---
+++ frontend/components/TodoItem.tsx
@@ -0,0 +1,61 @@
+import { updateTodo } from "@/features/requests";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
+
+const TodoItem = ({
+  id,
+  todo,
+  status,
+  priority,
+  created_at,
+}: {
+  id: number;
+  todo: string;
+  status: boolean;
+  priority: number;
+  created_at: string;
+}) => {
+  const client = useQueryClient();
+
+  const searchParams = useSearchParams();
+
+  const paramsSearch = searchParams.get("search") || null;
+  const paramsOrderBy = searchParams.get("orderby") || null;
+  const paramsOrder = searchParams.get("order") || null;
+  const paramsIsFinished = searchParams.get("finished") || null;
+  const paramsPage = Number(searchParams.get("page")) || 1;
+
+  const { mutate } = useMutation({
+    mutationFn: () => updateTodo({ id: String(id) }),
+    onSuccess() {
+      client.invalidateQueries({
+        queryKey: [
+          "todoFetch",
+          paramsSearch,
+          paramsPage,
+          paramsIsFinished,
+          paramsOrderBy,
+          paramsOrder,
+        ],
+      });
+    },
+  });
+
+  return (
+    <li className="block border rounded-md p-2 w-full">
+      <div className="flex w-full justify-between">
+        <h3>{todo}</h3>
+        <p>Priority: {priority}</p>
+      </div>
+      <h5 className={`${status ? "text-(--success)" : "opacity-50"} text-xs `}>
+        {status ? "Finished" : "In proccess"}
+      </h5>
+      <div className="flex items-center justify-between">
+        <p className="text-xs">{new Date(created_at).toDateString()}</p>
+        <button onClick={() => mutate()}>Update</button>
+      </div>
+    </li>
+  );
+};
+
+export default TodoItem;
---
+++ frontend/components/TodoList.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { getTodos } from "../features/requests";
+import {
+  useSearchParams,
+  // useRouter, usePathname
+} from "next/navigation";
+import TodoItem from "./TodoItem";
+import PaginationComponent from "./Pagination";
+import { Spinner } from "./ui/spinner";
+
+const TodoList = () => {
+  const searchParams = useSearchParams();
+
+  const paramsSearch = searchParams.get("search") || null;
+  const paramsOrderBy = searchParams.get("orderby") || null;
+  const paramsOrder = searchParams.get("order") || null;
+  const paramsIsFinished = searchParams.get("finished") || null;
+  const paramsPage = Number(searchParams.get("page")) || 1;
+
+  const { data, isFetched } = useQuery({
+    queryKey: [
+      "todoFetch",
+      paramsSearch,
+      paramsPage,
+      paramsIsFinished,
+      paramsOrderBy,
+      paramsOrder,
+    ],
+    queryFn: () =>
+      getTodos({
+        search: paramsSearch,
+        orderBy: paramsOrderBy,
+        order: paramsOrder as "ASC" | "DESC",
+        isFinished: paramsIsFinished,
+        page: paramsPage,
+      }),
+  });
+
+  const page = Math.ceil(data?.meta.allTodos / data?.meta.limit);
+
+  if (!isFetched)
+    return (
+      <div className="flex items-center justify-center flex-1">
+        <Spinner />
+      </div>
+    );
+
+  return (
+    <div className="flex flex-col flex-1 min-h-0 w-full mt-2">
+      {data.todos.length > 0 ? (
+        <ul className="flex flex-col max-md:flex-1 min-h-0 gap-3 list-none w-full overflow-auto">
+          {data.todos.map(
+            ({
+              id,
+              todo,
+              status,
+              priority,
+              created_at,
+            }: {
+              id: number;
+              todo: string;
+              status: boolean;
+              priority: number;
+              created_at: string;
+            }) => {
+              return (
+                <TodoItem
+                  key={id}
+                  id={id}
+                  created_at={created_at}
+                  priority={priority}
+                  status={status}
+                  todo={todo}
+                />
+              );
+            },
+          )}
+        </ul>
+      ) : (
+        <div className="flex items-center justify-center flex-1">
+          <p>No TO-DOs</p>
+        </div>
+      )}
+      {page > 0 && <PaginationComponent pages={page} />}
+    </div>
+  );
+};
+
+export default TodoList;
